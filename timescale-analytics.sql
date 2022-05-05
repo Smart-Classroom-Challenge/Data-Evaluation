132361eb10781ad787940377689d46a82687f4ac
Yvo Keller <keller.yvo@gmail.com>
2022-05-05 09:47:42 +0200

    using some timescale/postgres queries for feature

---
+++ timescale-analytics.sql
@@ -0,0 +1,44 @@
+-- overview: https://docs.timescale.com/timescaledb/latest/how-to-guides/query-data/advanced-analytic-queries/#histogram
+
+-- get median temperature for a measurement station
+SELECT percentile_cont(0.5)
+       WITHIN GROUP (ORDER BY temperature)
+FROM smartclassroom_dev.public.api_measurement
+WHERE fk_measurement_station_id = 1;
+
+-- moving average
+SELECT time,
+       AVG(temperature) OVER (ORDER BY time ROWS BETWEEN 9 PRECEDING AND CURRENT ROW)
+           AS smooth_temp
+FROM smartclassroom_dev.public.api_measurement
+WHERE fk_measurement_station_id = 1
+  and time > NOW() - INTERVAL '1 day'
+ORDER BY time DESC;
+
+-- delta values - only get values that have changed in result set
+SELECT time, temperature
+FROM (
+         SELECT time,
+                temperature,
+                temperature - LAG(temperature) OVER (ORDER BY time) AS diff
+         FROM smartclassroom_dev.public.api_measurement
+         WHERE fk_measurement_station_id = 1) ht
+WHERE diff IS NULL
+   OR diff != 0;
+
+-- get average temperature over a time buckets of 5 minutes
+SELECT time_bucket('5 minutes', time) AS five_min, round(avg(temperature), 2)
+FROM smartclassroom_dev.public.api_measurement
+WHERE fk_measurement_station_id = 1
+GROUP BY five_min
+ORDER BY five_min DESC
+LIMIT 100;
+
+-- histogram - get temperature values in 10 bins between 15° C and 30° C, which makes a bin range of 1.5°
+SELECT ms.name,
+       COUNT(*),
+       histogram(m.temperature, 15.0, 30.0, 10)
+FROM smartclassroom_dev.public.api_measurement m
+         INNER JOIN smartclassroom_dev.public.api_measurementstation ms ON m.fk_measurement_station_id = ms.id
+WHERE time > NOW() - INTERVAL '14 days'
+GROUP BY ms.name;
