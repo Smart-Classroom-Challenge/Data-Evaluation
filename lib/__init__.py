70976cb1ec5bd9ff5294dd0d86f1b4d06722d578
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2022-05-25 07:14:26 +0200

    add missing lib code

---
+++ lib/__init__.py
@@ -0,0 +1,1 @@
+from lib.offload import Offload
---
+++ lib/offload.py
@@ -0,0 +1,153 @@
+import psycopg2
+import configparser
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from datetime import datetime, timedelta
+
+class Offload:
+    plt.rcParams['timezone'] = "CET"
+    meteofile = "data/2022_05_17_meteoswiss.txt"
+
+    def __init__(self):
+        plt.rcParams['timezone'] = "CET"
+
+    def __read_config(self, filename = 'config.ini'):
+        config = configparser.ConfigParser()
+        config.read(filename)
+
+        db_hostname = config['DATABASE']['HOSTNAME']
+        db_database = config['DATABASE']['DATABASE']
+        db_username = config['DATABASE']['USERNAME']
+        db_password = config['DATABASE']['PASSWORD']
+
+        return db_hostname, db_database, db_username, db_password
+
+    def __get_connection(self):
+        db_hostname, db_database, db_username, db_password = self.__read_config()
+        return  psycopg2.connect(
+            host     = db_hostname,
+            database = db_database,
+            user     = db_username,
+            password = db_password)
+
+    def get_measurement(self, name, startDate, endDate):
+        con = self.__get_connection()
+
+        sql_stations = f"""
+            set timezone = 'CET'; 
+            select * from api_classroom
+            inner join api_measurementstation on api_classroom.id = api_measurementstation.fk_classroom_id
+            inner join api_measurement on api_measurementstation.id = api_measurement.fk_measurement_station_id
+            where api_classroom.name = '{name}' and time BETWEEN '{startDate}' AND '{endDate}';
+        """    
+        result = pd.read_sql_query(sql_stations, con)
+        
+        if not result.empty:
+            result["time"]        = result["time"].dt.tz_convert("CET")
+            result["updated_on"]  = result["updated_on"].dt.tz_convert("CET")
+            result["insert_time"] = result["insert_time"].dt.tz_convert("CET")
+        
+        con.close()
+        return result
+
+    def get_singe_measurement(self, name, datetime):
+        con = self.__get_connection()
+
+        sql_stations = f"""
+            set timezone = 'CET'; 
+            select * from api_classroom
+            inner join api_measurementstation on api_classroom.id = api_measurementstation.fk_classroom_id
+            inner join api_measurement on api_measurementstation.id = api_measurement.fk_measurement_station_id
+            where api_classroom.name = '{name}' and time = '2022-05-04 08:06:57.554084+02:00';
+        """    
+
+        result = pd.read_sql_query(self, sql_stations, con)
+        
+        if not result.empty:
+            result["time"]        = result["time"].dt.tz_convert("CET")
+            result["updated_on"]  = result["updated_on"].dt.tz_convert("CET")
+            result["insert_time"] = result["insert_time"].dt.tz_convert("CET")
+        
+        con.close()
+        return result
+
+    def get_measurement_for_timetable(self, name, stundenplan):
+        """
+        stundenplan: ist eine Liste mit einem dict zu jeder lektion, darin ist der start, ende und die personenanzahl enthalten.
+        """
+        start_date = datetime(2022, 4, 25)
+        dfs = []
+
+        for week in range(3):
+            for lektion in stundenplan:
+                date = start_date + timedelta(days=7*week + lektion[0])
+                start_time = date.replace(hour=lektion[1][0], minute=lektion[1][1])
+                end_time   = date.replace(hour=lektion[2][0], minute=lektion[2][1])
+                df = self.get_measurement(name, start_time, end_time)
+                if not df.empty:
+                    df['People'] = lektion[3]
+                    dfs.append(df)
+
+        return pd.concat(dfs)
+
+    def get_entrance(self, name, startDate, endDate):
+        con = self.__get_connection()
+
+        sql_stations = f"""
+            set timezone = 'CET'; 
+            select * from api_classroom
+            inner join api_measurementstation on api_classroom.id = api_measurementstation.fk_classroom_id
+            inner join api_entranceevent on api_measurementstation.id = api_entranceevent.fk_measurement_station_id
+            where time BETWEEN '{startDate}' AND '{endDate}' and api_classroom.name = '{name}';
+        """    
+        result = pd.read_sql_query(sql_stations, con)
+        
+        result["time"]        = result["time"].dt.tz_convert("CET")
+        result["updated_on"]  = result["updated_on"].dt.tz_convert("CET")
+        result["insert_time"] = result["insert_time"].dt.tz_convert("CET")
+        
+        con.close()
+        
+        result = result.sort_values(['time'])
+        return result
+
+    def get_meteo(self, startDate, endDate):
+        meteo = pd.read_csv(self.meteofile, delimiter = ';')
+        meteo.columns = ["Station",
+                    "Time", 
+                    "Lufttemperatur 2 m über Boden; Momentanwert in °C", 
+                    "Niederschlag; gleitende Stundensumme (über 6 Zehnminutenintervalle) in mm", 
+                    "Relative Luftfeuchtigkeit 2 m über Boden; Momentanwert in %",
+                    "Windgeschwindigkeit; Zehnminutenmittel in %"]
+
+        meteo["Time"] = pd.to_datetime(meteo["Time"], format = "%Y%m%d%H%M")
+        meteo["Time"] = meteo["Time"] + pd.Timedelta(hours = 2)
+        meteo = meteo.set_index("Time")
+
+        if startDate != None:
+            meteo = meteo[meteo.index >= startDate]
+
+        if endDate != None:
+            meteo = meteo.loc[meteo.index <= endDate]
+
+        return meteo
+
+    def get_people_count_overtime(self, entrance_list):
+        people_count     = 0
+        lst_people_count = []
+        lst_time         = []
+        array = []
+        
+        for index, row in entrance_list.iterrows():
+            people_count += row['change']
+            ttime = row['time']
+            lst_people_count.append(people_count)
+            lst_time.append(ttime) 
+            
+            d = dict({'people_count': people_count, 'time': ttime})
+            array.append(d)
+
+        df = pd.DataFrame.from_dict(array,orient='columns')
+        df = df.sort_values(['time'], ascending=True)
+        return df
